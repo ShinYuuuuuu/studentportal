835bcc799469177c7ca710fcf84b1427d2e3ab0c
ShinYuuuuuu <dhongzkierey@gmail.com>
2026-04-03 13:08:57 +0800

    Change demo user name to Reynante Yu

--- public/index.php
+++ public/index.php
@@ -19,8 +19,8 @@ if ($page === 'login' && $_SERVER['REQUEST_METHOD'] === 'POST') {
         $_SESSION['user'] = [
             'id' => 1,
             'student_id' => '2023-00123',
-            'first_name' => 'Classic',
-            'last_name' => 'John Doe',
+            'first_name' => 'Reynante',
+            'last_name' => 'Yu',
             'program' => 'BS Computer Science',
             'year_level' => '3rd Year',
             'section' => 'CS-3A'
